95f10f5a2532dc41b033ad3595d6c75d00d44b1e
jidraph kimachia <jidraphkimachia1@gmail.com>
2022-03-05 00:19:59 +0300

    added js arrays

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
 
 
 
-document.getElementById("female")
+const days = ["Sunday" , "Monday" , "Tuesday" , "Wednesday" , "Thursday" , "Friday" , "Saturday"]
+const male ={
+    Sunday : "Kwasi",
+    Monday : "Kwadwo",
+    Tuesday : "Kwabena",
+    Wednesday : "Kwaku",
+    Thursday : "Yaw",
+    Friday : "Kofi",
+    Saturday : "Kwame"
+}
+
+const female={
+    Sunday : "Akosua",
+    Monday : "Adwoa",
+    Tuesday : "Abenna",
+    Wednesday : "Akua",
+    Thursday : "Yaa",
+    Friday : "Afua",
+    Saturday : "Ama"
+}
+
+
+
