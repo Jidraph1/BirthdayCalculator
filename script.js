0dbc38bf016613676cf41588ba57f5b2df6f611c
jidraph kimachia <jidraphkimachia1@gmail.com>
2022-03-07 15:51:08 +0300

    final adjustments

--- script.js
+++ script.js
@@ -24,17 +24,17 @@ const female= {
 const form = document.getElementById('form')
 
 form.addEventListener('submit', function (event) {
-    event.preventDefault();
-    const gender = document.querySelector('input[name="gender"]:checked').value
-    const dob = document.getElementById('DOB').value
-    const result = document.getElementById('result')
-    const date = new Date(dob)
-    const day = date.getDay()
-    const dayOfTheWeek = days[day]
+        event.preventDefault();
+        const gender = document.querySelector('input[name="gender"]:checked').value
+        const dob = document.getElementById('DOB').value
+        const result = document.getElementById('result')
+        const date = new Date(dob)
+        const day = date.getDay()
+        const dayOfTheWeek = days[day]
 
-    if(gender === 'Male') {
-         alert( ' Your Akan name is ' + male[dayOfTheWeek])
-    } else {
-        alert( ' Your Akan name is ' + female[dayOfTheWeek])
-    }
-}
+        if (gender === 'Male') {
+            alert('Your Akan name is ' + male[dayOfTheWeek])
+        } else {
+            alert('Your Akan name is ' + female[dayOfTheWeek])
+        }
+    })
